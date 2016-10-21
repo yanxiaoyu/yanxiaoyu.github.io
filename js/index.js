53a6d0e70e5207b0eaffff12f563fbabb4734a8c
ice <1984199228@qq.com>
2016-10-21 13:00:50 +0800

    2

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
 var vm = avalon.define({
 	$id:"info",
-	list:[],
-	getInfo:function(){
-		$.get("../data/json_1.json",function(ret){
-			vm.list = ret;
-			
-			console.log(ret);
-		});
-	},
-	showName:function(){
-		console.log(vm.list);
-	}
-});
-
-$(function(){
-	vm.getInfo();
-	vm.showName();
+	list:[
+			{
+				id:"1",
+				title:"这是标题"
+			},
+			{
+				id:"2",
+				title:"这是标题22222"
+			}
+		]
 });
