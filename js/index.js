121b8a3e751a60a5ff459888b0248150ec7fbde9
ice <1984199228@qq.com>
2016-10-21 11:46:44 +0800

    换avalon

--- js/index.js
+++ js/index.js
@@ -1,24 +1,10 @@
-function getData(){
-	var mydata;
-	$.get("../data/json_1.json",function(ret){
-		console.log(ret);
-		mydata = ret;
-		console.log(mydata);
-	});
-	console.log(mydata);
-	return mydata;
-}
-var col_1_data = getData();
-console.log(col_1_data);
-var vm = new Vue({
-	el:"#info",
-	data:{
-		col_1:col_1_data,
-		msg:"我是小鱼"
-	},
-	methods:{
-		
+var vm = avalon.define({
+	$id:"info",
+	list:[],
+	getInfo:function(){
+		$.get("../data/json_1.json",function(ret){
+			vm.list = ret;
+		});
 	}
 });
-
 
